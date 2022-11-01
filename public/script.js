5d4ba55adcc4244cd11d8e5d9b112fb3b849c644
Alistair Horwood <alistair.horwood@gmail.com>
2022-11-02 00:10:42 +1030

    Further development of styles and javascript

--- public/script.js
+++ public/script.js
@@ -1,11 +1,26 @@
 function hide_student_details(){
-    document.getElementById('student_details').hidden = false;
-    document.getElementById('LoggingFAN').required = false;
-    document.getElementById('LoggingStudentID').required = false;
+    // document.getElementById('student_details').hidden = false;
+    // document.getElementById('LoggingFAN').required = false;
+    // document.getElementById('LoggingStudentID').required = false;
   }
 
 function show_student_details(){
-   document.getElementById('student_details').hidden = true;
-   document.getElementById('LoggingFAN').required = true;
-   document.getElementById('LoggingStudentID').required = true;
+  //  document.getElementById('student_details').hidden = true;
+  //  document.getElementById('LoggingFAN').required = true;
+  //  document.getElementById('LoggingStudentID').required = true;
 }
+
+
+function initial_questions(){
+  document.getElementById('initial_questions').hidden = true;
+  document.getElementById('secondary_questions').hidden = false;
+}
+
+function secondary_questions(){
+  console.log("secondary_questions");
+  document.getElementById('initial_questions').hidden = true;
+  document.getElementById('secondary_questions').hidden = false;
+}
+
+
+
